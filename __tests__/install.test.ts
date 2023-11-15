2b645c41705f215d702bd51ad07390731058f8e6
Aleksandar Stojanov <me@fnd.works>
2023-11-15 15:57:07 +0100

    Initial release
    Signed-off-by: Aleksandar Stojanov <me@fnd.works>

---
+++ __tests__/install.test.ts
@@ -0,0 +1,177 @@
+import * as os from 'os'
+import * as fs from 'fs'
+import * as path from 'path'
+import * as util from 'util'
+import { getHelmDocs, installHelmDocs, findHelmDocs } from '../src/install'
+import * as tc from '@actions/tool-cache'
+
+jest.mock('os')
+jest.mock('fs')
+jest.mock('util')
+jest.mock('@actions/tool-cache', () => ({
+  downloadTool: jest.fn().mockResolvedValue('/mocked/path/tool.tgz'),
+  find: jest.fn().mockReturnValue('/mocked/path/cachedTool'),
+  extractTar: jest.fn().mockResolvedValue('/mocked/path/untarredTool'),
+  cacheDir: jest.fn().mockResolvedValue('/mocked/path/cachedDir')
+}))
+
+describe('getHelmDocs tests', () => {
+  let helmDocs: string
+  let version: string
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+
+    helmDocs = 'helm-docs'
+    version = 'v1.11.3'
+  })
+
+  it('should return the correct url for Windows_NT and arm64 arch', () => {
+    const osType = 'windows'
+    const osArch = 'arm64'
+    const windowsURL = util.format(
+      'https://github.com/losisin/%s/releases/download/%s/%s_%s_%s_%s.tgz',
+      helmDocs,
+      version,
+      helmDocs,
+      version.substring(1),
+      osType,
+      osArch
+    )
+    ;(os.type as jest.Mock).mockReturnValue(osType)
+    ;(os.arch as jest.Mock).mockReturnValue(osArch)
+
+    const result = getHelmDocs(version)
+
+    expect(result).toBe(windowsURL)
+  })
+
+  it('should return the correct url for Linux and arm64 arch', () => {
+    const osType = 'linux'
+    const osArch = 'arm64'
+    const windowsURL = util.format(
+      'https://github.com/losisin/%s/releases/download/%s/%s_%s_%s_%s.tgz',
+      helmDocs,
+      version,
+      helmDocs,
+      version.substring(1),
+      osType,
+      osArch
+    )
+    ;(os.type as jest.Mock).mockReturnValue(osType)
+    ;(os.arch as jest.Mock).mockReturnValue(osArch)
+
+    const result = getHelmDocs(version)
+
+    expect(result).toBe(windowsURL)
+  })
+
+  it('should return the correct url for MAC_OS and amd64 arch', () => {
+    const osType = 'darwin'
+    const osArch = 'amd64'
+    const windowsURL = util.format(
+      'https://github.com/losisin/%s/releases/download/%s/%s_%s_%s_%s.tgz',
+      helmDocs,
+      version,
+      helmDocs,
+      version.substring(1),
+      osType,
+      osArch
+    )
+    ;(os.type as jest.Mock).mockReturnValue(osType)
+    ;(os.arch as jest.Mock).mockReturnValue(osArch)
+
+    const result = getHelmDocs(version)
+
+    expect(result).toBe(windowsURL)
+  })
+})
+
+describe('installHelmDocs', () => {
+  let helmDocs: string
+  let version: string
+
+  beforeEach(() => {
+    jest.clearAllMocks()
+
+    helmDocs = 'helm-docs'
+    version = 'v1.11.3'
+  })
+
+  it('installs helm-docs if it is not already cached', async () => {
+    ;(os.type as jest.Mock).mockReturnValue('Linux')
+    ;(tc.find as jest.Mock).mockReturnValue('')
+    ;(tc.downloadTool as jest.Mock).mockResolvedValue('/downloads/tool.tgz')
+    ;(fs.chmodSync as jest.Mock).mockReturnValue(null)
+    ;(tc.extractTar as jest.Mock).mockResolvedValue('/extracts/tool')
+    ;(tc.cacheDir as jest.Mock).mockResolvedValue('/cached/tool')
+    ;(fs.readdirSync as jest.Mock).mockReturnValue(['other', 'helm-docs'])
+    ;(fs.statSync as jest.Mock).mockReturnValue({ isDirectory: () => false })
+
+    const result = await installHelmDocs(version)
+
+    expect(result).toBe('/cached/tool/helm-docs')
+    expect(tc.find).toHaveBeenCalledWith(helmDocs, version)
+    expect(tc.downloadTool).toHaveBeenCalledWith(getHelmDocs(version))
+    expect(fs.chmodSync).toHaveBeenCalledWith('/downloads/tool.tgz', '777')
+    expect(tc.extractTar).toHaveBeenCalledWith('/downloads/tool.tgz')
+    expect(tc.cacheDir).toHaveBeenCalledWith(
+      '/extracts/tool',
+      helmDocs,
+      version
+    )
+  })
+
+  it('finds helm-docs from the cache if it is already cached', async () => {
+    ;(os.type as jest.Mock).mockReturnValue('Linux')
+    ;(tc.find as jest.Mock).mockReturnValue('/cached/tool')
+    ;(fs.statSync as jest.Mock).mockReturnValue({ isDirectory: () => false })
+
+    const result = await installHelmDocs(version)
+
+    expect(result).toBe('/cached/tool/helm-docs')
+    expect(tc.find).toHaveBeenCalledWith(helmDocs, version)
+    expect(tc.downloadTool).not.toHaveBeenCalled()
+    expect(tc.extractTar).not.toHaveBeenCalled()
+    expect(tc.cacheDir).not.toHaveBeenCalled()
+  })
+})
+describe('findHelmDocs', () => {
+  it('finds the correct file in the directory', () => {
+    ;(os.type as jest.Mock).mockReturnValue('Linux')
+    ;(fs.readdirSync as jest.Mock).mockReturnValue(['helm-docs'])
+    ;(fs.statSync as jest.Mock).mockReturnValue({ isDirectory: () => false })
+
+    const result = findHelmDocs('/some/dir')
+
+    expect(result).toBe('/some/dir/helm-docs')
+    expect(fs.chmodSync).toHaveBeenCalledWith('/some/dir', '777')
+  })
+
+  it('ignores subdirectories', () => {
+    const directoryName = 'a_subdirectory'
+    const helmDocsFolder = '/some/dir'
+
+    ;(os.type as jest.Mock).mockReturnValue('Linux')
+    ;(fs.readdirSync as jest.Mock).mockReturnValue([directoryName])
+    ;(fs.statSync as jest.Mock).mockReturnValue({ isDirectory: () => true })
+
+    expect(() => findHelmDocs(helmDocsFolder)).toThrow(
+      `helm-docs executable not found in path: ${helmDocsFolder}`
+    )
+    expect(fs.statSync).toHaveBeenCalledWith(
+      path.join(helmDocsFolder, directoryName)
+    )
+    expect(fs.statSync).toHaveBeenCalledTimes(1)
+  })
+
+  it('throws error when plugin file is not found', () => {
+    ;(os.type as jest.Mock).mockReturnValue('Linux')
+    ;(fs.readdirSync as jest.Mock).mockReturnValue(['notSchema'])
+    ;(fs.statSync as jest.Mock).mockReturnValue({ isDirectory: () => false })
+
+    expect(() => findHelmDocs('/some/dir')).toThrow(
+      'helm-docs executable not found in path: /some/dir'
+    )
+  })
+})
---
+++ src/main.ts
@@ -0,0 +1,73 @@
+import * as path from 'path'
+import { installHelmDocs } from './install'
+import * as core from '@actions/core'
+import * as exec from '@actions/exec'
+import { simpleGit } from 'simple-git'
+
+const version = 'v1.11.3'
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
+  try {
+    const chartSearchRoot =
+      core.getInput('chart-search-root') || '__tests__/fixtures'
+    const valuesFile = core.getInput('values-file') || 'values.yaml'
+    const outputFile = core.getInput('output-file') || 'README.md'
+    const gitPush = core.getInput('git-push')
+    const gitPushUserName = core.getInput('git-push-user-name')
+    const gitPushUserEmail = core.getInput('git-push-user-email')
+    const gitCommitMessage = core.getInput('git-commit-message')
+    const failOnDiff = core.getInput('fail-on-diff')
+
+    core.startGroup(`Downloading helm-docs ${version}`)
+    const cachedPath = await installHelmDocs(version)
+    core.endGroup()
+
+    process.env['PATH']?.startsWith(path.dirname(cachedPath)) ||
+      core.addPath(path.dirname(cachedPath))
+
+    core.info(`helm-docs binary '${version}' has been cached at ${cachedPath}`)
+    core.setOutput('helm-docs', cachedPath)
+
+    await exec.exec('helm-docs', [
+      '--values-file',
+      valuesFile,
+      '--output-file',
+      outputFile,
+      '--chart-search-root',
+      chartSearchRoot
+    ])
+
+    const git = simpleGit()
+    const statusSummary = await git.status()
+
+    const outputStatus = statusSummary.files.find(
+      file => file.path === outputFile
+    )
+    if (outputStatus) {
+      switch (true) {
+        case failOnDiff === 'true':
+          core.setFailed(`'${outputFile}' has changed`)
+          break
+        case gitPush === 'true':
+          await git.addConfig('user.name', gitPushUserName)
+          await git.addConfig('user.email', gitPushUserEmail)
+          await git.add([outputFile])
+          await git.commit(gitCommitMessage)
+          await git.push()
+          core.info(`Pushed '${outputFile}' to the branch.`)
+          break
+        default:
+          core.info(`'${outputFile}' has changed, but no action was requested.`)
+      }
+    } else {
+      core.info(`'${outputFile}' is up to date.`)
+    }
+  } catch (error) {
+    // Fail the workflow run if an error occurs
+    if (error instanceof Error) core.setFailed(error.message)
+  }
+}
