3ab956521a42c3e7ba65aff19817f5107caefddb
Aleksandar Stojanov <me@fnd.works>
2023-12-26 09:21:13 +0100

    bump version to latest plugin v1.12.0
    Signed-off-by: Aleksandar Stojanov <me@fnd.works>

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 import { simpleGit } from 'simple-git'
 
-const version = 'v1.11.3'
+const version = 'v1.12.0'
 
 /**
  * The main function for the action.
